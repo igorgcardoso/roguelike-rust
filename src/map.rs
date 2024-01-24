855aade1e9e9e7182d57c77ecf26d5c875557e2c
Igor Cardoso <igorgcardoso7@gmail.com>
2024-01-24 11:37:07 -0300

    Chapter 23 - Refactor: Generic Map Interface

--- src/map.rs
+++ src/map.rs
@@ -18,7 +18,6 @@ pub enum TileType {
 #[derive(Default, Serialize, Deserialize, Clone)]
 pub struct Map {
     pub tiles: Vec<TileType>,
-    pub rooms: Vec<Rect>,
     pub width: i32,
     pub height: i32,
     pub revealed_tiles: Vec<bool>,
@@ -33,34 +32,9 @@ pub struct Map {
 }
 
 impl Map {
-    pub fn xy_idx(&self, x: i32, y: i32) -> usize {
-        (y as usize * self.width as usize) + x as usize
-    }
-
-    fn apply_horizontal_tunnel(&mut self, x1: i32, x2: i32, y: i32) {
-        for x in min(x1, x2)..=max(x1, x2) {
-            let idx = self.xy_idx(x, y);
-            if idx > 0 && idx < MAPCOUNT {
-                self.tiles[idx as usize] = TileType::Floor;
-            }
-        }
-    }
-
-    fn apply_vertical_tunnel(&mut self, y1: i32, y2: i32, x: i32) {
-        for y in min(y1, y2)..=max(y1, y2) {
-            let idx = self.xy_idx(x, y);
-            if idx > 0 && idx < MAPCOUNT {
-                self.tiles[idx as usize] = TileType::Floor;
-            }
-        }
-    }
-
-    /// Makes a new map using the algorithm from http://rogueliketutorials.com/tutorials/tcod/part-3/
-    /// This gives a handful of random rooms and corridors joining them together.
-    pub fn new_map_rooms_and_corridors(new_depth: i32) -> Self {
-        let mut map = Self {
+    pub fn new(new_depth: i32) -> Self {
+        Self {
             tiles: vec![TileType::Wall; MAPCOUNT],
-            rooms: Vec::new(),
             width: MAPWIDTH as i32,
             height: MAPHEIGHT as i32,
             revealed_tiles: vec![false; MAPCOUNT],
@@ -69,61 +43,10 @@ impl Map {
             tile_content: vec![Vec::new(); MAPCOUNT],
             depth: new_depth,
             bloodstains: HashSet::new(),
-        };
-
-        const MAX_ROOMS: i32 = 30;
-        const MIN_SIZE: i32 = 6;
-        const MAX_SIZE: i32 = 10;
-
-        let mut rng = rltk::RandomNumberGenerator::new();
-
-        for _ in 0..MAX_ROOMS {
-            let w = rng.range(MIN_SIZE, MAX_SIZE);
-            let h = rng.range(MIN_SIZE, MAX_SIZE);
-            let x = rng.roll_dice(1, MAPWIDTH as i32 - w - 1) - 1;
-            let y = rng.roll_dice(1, MAPHEIGHT as i32 - h - 1) - 1;
-            let new_room = Rect::new(x, y, w, h);
-            let mut ok = true;
-            map.rooms.iter().any(|other_room| {
-                if new_room.intersect(other_room) {
-                    ok = false;
-                    true
-                } else {
-                    false
-                }
-            });
-            if ok {
-                map.apply_room_to_map(&new_room);
-
-                if !map.rooms.is_empty() {
-                    let (new_x, new_y) = new_room.center();
-                    let (prev_x, prev_y) = map.rooms[map.rooms.len() - 1].center();
-                    if rng.rand() {
-                        map.apply_horizontal_tunnel(prev_x, new_x, prev_y);
-                        map.apply_vertical_tunnel(prev_y, new_y, new_x);
-                    } else {
-                        map.apply_vertical_tunnel(prev_y, new_y, prev_x);
-                        map.apply_horizontal_tunnel(prev_x, new_x, new_y);
-                    }
-                }
-
-                map.rooms.push(new_room);
-            }
         }
-        let stairs_position = map.rooms[map.rooms.len() - 1].center();
-        let stairs_idx = map.xy_idx(stairs_position.0, stairs_position.1);
-        map.tiles[stairs_idx] = TileType::DownStairs;
-
-        map
     }
-
-    fn apply_room_to_map(&mut self, room: &Rect) {
-        for y in room.y1 + 1..=room.y2 {
-            for x in room.x1 + 1..=room.x2 {
-                let idx = self.xy_idx(x, y);
-                self.tiles[idx] = TileType::Floor;
-            }
-        }
+    pub fn xy_idx(&self, x: i32, y: i32) -> usize {
+        (y as usize * self.width as usize) + x as usize
     }
 
     fn is_exit_valid(&self, x: i32, y: i32) -> bool {
---
+++ src/map_builders/mod.rs
@@ -0,0 +1,18 @@
+mod common;
+mod simple_builder;
+
+use super::{spawner, Map, Position, Rect, TileType};
+use common::*;
+use simple_builder::SimpleMapBuilder;
+use specs::prelude::*;
+
+pub trait MapBuilder {
+    fn build_map(&mut self);
+    fn spawn_entities(&mut self, ecs: &mut World);
+    fn get_map(&mut self) -> Map;
+    fn get_starting_position(&mut self) -> Position;
+}
+
+pub fn random_builder(new_depth: i32) -> Box<dyn MapBuilder> {
+    Box::new(SimpleMapBuilder::new(new_depth))
+}
