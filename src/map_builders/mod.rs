603edbdb3ac63630c53fd5ae4331b2857491b82f
Igor Cardoso <igorgcardoso7@gmail.com>
2024-02-04 23:31:56 -0300

    Chapter 28 - Drunkard's Walk Maps

--- src/map_builders/mod.rs
+++ src/map_builders/mod.rs
@@ -2,14 +2,18 @@ mod bsp_dungeon;
 mod bsp_interior;
 mod cellular_automata;
 mod common;
+mod drunkward;
 mod simple_builder;
 
-use self::bsp_interior::BspInteriorBuilder;
-
+use self::{
+    bsp_interior::BspInteriorBuilder,
+    drunkward::{DrunkSpawnMode, DrunkardSettings},
+};
 use super::{spawner, Map, Position, Rect, TileType, SHOW_MAPGEN_VISUALIZER};
 use bsp_dungeon::BspDungeonBuilder;
 use cellular_automata::CellularAutomataBuilder;
 use common::*;
+use drunkward::DrunkardsWalkBuilder;
 use simple_builder::SimpleMapBuilder;
 use specs::prelude::*;
 
@@ -24,11 +28,14 @@ pub trait MapBuilder {
 
 pub fn random_builder(new_depth: i32) -> Box<dyn MapBuilder> {
     let mut rng = rltk::RandomNumberGenerator::new();
-    let builder = rng.roll_dice(1, 4);
+    let builder = rng.roll_dice(1, 7);
     match builder {
         1 => Box::new(BspDungeonBuilder::new(new_depth)),
         2 => Box::new(BspInteriorBuilder::new(new_depth)),
         3 => Box::new(CellularAutomataBuilder::new(new_depth)),
+        4 => Box::new(DrunkardsWalkBuilder::open_area(new_depth)),
+        5 => Box::new(DrunkardsWalkBuilder::open_halls(new_depth)),
+        6 => Box::new(DrunkardsWalkBuilder::winding_passages(new_depth)),
         _ => Box::new(SimpleMapBuilder::new(new_depth)),
     }
 }
